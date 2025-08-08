c004e1421e98283b0912081e3c44bf11d2d2aa9a
ServiceBot Developer <developer@servicebot.com>
2025-08-08 11:34:30 +0700

    修复当文本超4000的bug

--- src/handlers/handleSinglePhoto.ts
+++ src/handlers/handleSinglePhoto.ts
@@ -1,6 +1,7 @@
 import { type MyContext } from '../utils/types.js';
 import { handleOCR } from './handleOCR.js';
 import { handlePolish } from './handlePolish.js';
+import { safeReply } from '../utils/safeReply.js';
 
 export async function handleSinglePhoto(ctx: MyContext) {
     if (!ctx.message) return;
@@ -11,9 +12,13 @@ export async function handleSinglePhoto(ctx: MyContext) {
     const rawText = await handleOCR(process.env.BOT_TOKEN!, fileId);
     const polished = await handlePolish(rawText);
 
+
+    //润色完毕
     ctx.session.lastPolishedText = polished;
 
-    await ctx.reply(`✨ 澎色完成内容：\n\n${polished}`, { parse_mode: 'Markdown' });
+    //如果文本超过2000就会出错
+    // await ctx.reply(`✨ 澎色完成内容：\n\n${polished}`, { parse_mode: 'Markdown' });
+    await safeReply(`✨ 澎色完成内容：\n\n${polished}`, { parse_mode: 'Markdown' });
     await ctx.reply('是否保存到 Flomo？', {
         reply_markup: {
             inline_keyboard: [
