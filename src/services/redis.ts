2c938079ba966cba1ff103d1ca4c2b1f771003a3
ServiceBot Developer <developer@servicebot.com>
2025-08-30 01:02:04 +0700

    更新Redis连接配置，使用Upstash Redis服务

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,6 +1,6 @@
 // src/services/redis.ts
 import { Redis } from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL ?? "redis://localhost:6379", {
+export const redis = new Redis("rediss://default:AVfTAAIncDEyY2FmYTgyMzM0ZmY0ZGQ1OTY1NjM2NmVlNWNhNDg2NXAxMjI0ODM@immune-buffalo-22483.upstash.io:6379", {
     maxRetriesPerRequest: null
 });
